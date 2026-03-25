5c8ca934cdb7fe66b3f3b2f2a6fd1bc945b09e6c
ismailchtourou <ichtourou45@gmail.com>
2026-03-25 14:09:23 +0100

    Completed number Button fonction Add result Label

--- calculator.py
+++ calculator.py
@@ -2,53 +2,122 @@ from tkinter import *
 equation=""
 calwin=Tk()
 #button fonction
-def Butt1() :
+            #####Important#####
+#######Lezem nl9wa methode n5tsro biha he4a el kol########
+##jrbt n3ml haka ama m5dmtch
+#def Buttonpr(n) :
+#    global equation
+#    equation=equation+str(n)
+#    inLabel.config(text=equation)
+#oneButton=Button(calwin,text="1",
+#                font=("Arial",60,"bold"),
+#                command=Buttonpr(1),
+#               )
+#twoButton=Button(calwin,text="2",
+#                font=("Arial",60,"bold"),
+#                command=Buttonpr(2),
+#               )
+def Button0() :
     global equation
-    n1 = 1
-    equation = equation + str(n1)
+    n = 0
+    equation = equation + str(n)
     inLabel.config(text=equation)
-def Butt2() :
+
+def Button1() :
     global equation
-    n2 = 2
-    equation=equation+str(n2)
+    n = 1
+    equation = equation + str(n)
     inLabel.config(text=equation)
+def Button2() :
+    global equation
+    n = 2
+    equation=equation+str(n)
+    inLabel.config(text=equation)
+def Button2() :
+    global equation
+    n = 2
+    equation=equation+str(n)
+    inLabel.config(text=equation)
+def Button3() :
+    global equation
+    n = 3
+    equation=equation+str(n)
+    inLabel.config(text=equation)
+def Button4() :
+    global equation
+    n = 4
+    equation=equation+str(n)
+    inLabel.config(text=equation)
+def Button5() :
+    global equation
+    n = 5
+    equation=equation+str(n)
+    inLabel.config(text=equation)
+def Button6() :
+    global equation
+    n = 6
+    equation=equation+str(n)
+    inLabel.config(text=equation)
+def Button7() :
+    global equation
+    n = 7
+    equation=equation+str(n)
+    inLabel.config(text=equation)
+def Button8() :
+    global equation
+    n = 8
+    equation=equation+str(n)
+    inLabel.config(text=equation)
+def Button9() :
+    global equation
+    n = 9
+    equation=equation+str(n)
+    inLabel.config(text=equation)
+
 #Button from zero to nine
 zeroButton=Button(calwin,text="0",
                 font=("Arial",60,"bold"),
+                command=Button0 ,
                 )
 oneButton=Button(calwin,text="1",
                 font=("Arial",60,"bold"),
-                command=Butt1,
+                command=Button1,
                 )
 twoButton=Button(calwin,text="2",
                 font=("Arial",60,"bold"),
-                command=Butt2 ,
+                command=Button2 ,
                 )
 threeButton=Button(calwin,text="3",
                 font=("Arial",60,"bold"),
+                command=Button3 ,
                 )
 fourButton=Button(calwin,text="4",
                 font=("Arial",60,"bold"),
+                command=Button4,
                 )
 fiveButton=Button(calwin,text="5",
                 font=("Arial",60,"bold"),
+                command=Button5 ,
                 )
 sixButton=Button(calwin,text="6",
                 font=("Arial",60,"bold"),
+                command=Button6,
                 )
 sevenButton=Button(calwin,text="7",
                 font=("Arial",60,"bold"),
+                command=Button7 ,
                 )
 eightButton=Button(calwin,text="8",
                 font=("Arial",60,"bold"),
+                command=Button8 ,
                 )
 nineButton=Button(calwin,text="9",
                 font=("Arial",60,"bold"),
+                command=Button9 ,
                 )
 #lablels
-inLabel= Label(calwin,font=("Arial",50,"bold"))
-
-
+inLabel= Label(calwin,font=("Arial",50,"bold"),bd=10,relief="sunken")
+ResultLabel=Label(calwin,font=("Arial",50,"bold"),bd=10,relief="sunken")
 
 #other Button
 pointButton=Button(calwin,text=".",
@@ -73,23 +142,25 @@ calwin.title(" SE Calculator")
 calwin.geometry("380x640")
 calwin.config(background="#4b4c4f")
 
-zeroButton.grid(row=4,column=0,columnspan=2,pady=(0, 0),sticky=EW)
-pointButton.grid(row=4,column=2,pady=(0, 0),sticky=EW)
-oneButton.grid(row=3,column=0,pady=(0, 0))
-twoButton.grid(row=3,column=1,pady=(0, 0))
-threeButton.grid(row=3,column=2,pady=(0, 0))
-fourButton.grid(row=2,column=0,pady=(0, 0))
-fiveButton.grid(row=2,column=1,pady=(0, 0))
-sixButton.grid(row=2,column=2,pady=(0, 0))
-sevenButton.grid(row=1,column=0,pady=(0, 0))
-eightButton.grid(row=1,column=1,pady=(0, 0))
-nineButton.grid(row=1,column=2,pady=(0, 0))
+zeroButton.grid(row=5,column=0,columnspan=2,pady=(0, 0),sticky=EW)
+pointButton.grid(row=5,column=2,pady=(0, 0),sticky=EW)
+oneButton.grid(row=4,column=0,pady=(0, 0))
+twoButton.grid(row=4,column=1,pady=(0, 0))
+threeButton.grid(row=4,column=2,pady=(0, 0))
+fourButton.grid(row=3,column=0,pady=(0, 0))
+fiveButton.grid(row=3,column=1,pady=(0, 0))
+sixButton.grid(row=3,column=2,pady=(0, 0))
+sevenButton.grid(row=2,column=0,pady=(0, 0))
+eightButton.grid(row=2,column=1,pady=(0, 0))
+nineButton.grid(row=2,column=2,pady=(0, 0))
 
-inLabel.grid(row=0,column=0,columnspan=6,pady=(150,0),sticky=EW)
-equalsButton.grid(row=3,column=3,pady=(0, 0))
+inLabel.grid(row=0,column=0,columnspan=6,pady=(50,0),sticky=EW)
+ResultLabel.grid(row=1,column=0,columnspan=6,pady=(0,0),sticky=EW)
+
+equalsButton.grid(row=4,column=3,pady=(0, 0))
 equalsButton.config(bg="#62d3f5")
-plusButton.grid(row=2,column=3,pady=(0, 0))
-minesButton.grid(row=1,column=3,pady=(0, 0) , sticky=EW)
-multiplieButton.grid(row=2,column=4,pady=(0, 0) , sticky=EW)
+plusButton.grid(row=3,column=3,pady=(0, 0))
+minesButton.grid(row=2,column=3,pady=(0, 0) , sticky=EW)
+multiplieButton.grid(row=3,column=4,pady=(0, 0) , sticky=EW)
 calwin.mainloop()
 
